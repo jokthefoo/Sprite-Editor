f5dd6edac6a1ee9c5edbfacd6e630e3e913c99e5
jokthefoo <alexkofford@gmail.com>
2016-11-07 22:55:31 -0700

    Removed trace of selection tool
    title

--- Sprite_Editor_Extreme/model.cpp
+++ Sprite_Editor_Extreme/model.cpp
@@ -3,7 +3,6 @@
 #include <eraser.h>
 #include <fillbucket.h>
 #include <polygonbrush.h>
-#include <selectionTool.h>
 
 
 //This class represents the project as a collection of frames and holds all configuration settings for the
